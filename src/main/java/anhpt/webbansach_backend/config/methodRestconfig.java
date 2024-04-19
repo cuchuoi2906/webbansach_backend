1f1735eff28e3638e7e87edd505eb3b69c42b52d
Phạm tuấn anh <p.anhft@gmail.com>
2024-04-19 16:56:01 +0700

    a

--- src/main/java/anhpt/webbansach_backend/config/methodRestconfig.java
+++ src/main/java/anhpt/webbansach_backend/config/methodRestconfig.java
@@ -23,5 +23,8 @@ public class methodRestconfig implements RepositoryRestConfigurer {
         // Cho phép trả về id
         config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
         // config.exposeIdsFor(TheLoai.class);
+
+        // Config Cross
+        cors.addMapping("/**").allowedOrigins(url).allowedMethods("GET","POST","PUT","DELETE");
     }
 }
