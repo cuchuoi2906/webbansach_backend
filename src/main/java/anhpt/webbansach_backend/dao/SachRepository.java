d12a554751ed0ade603fac2dbb6030ece1792c2b
Phạm tuấn anh <p.anhft@gmail.com>
2024-05-06 16:56:51 +0700

    a

--- src/main/java/anhpt/webbansach_backend/dao/SachRepository.java
+++ src/main/java/anhpt/webbansach_backend/dao/SachRepository.java
@@ -2,11 +2,14 @@ package anhpt.webbansach_backend.dao;
 
 import anhpt.webbansach_backend.entity.DonHang;
 import anhpt.webbansach_backend.entity.Sach;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "sach")
 public interface SachRepository extends JpaRepository<Sach,Integer> {
-
+    Page<Sach> findByTenSachContaining(@RequestParam("tenSach") String tenSach, Pageable pageable);
 }
